16cbe5c81fd9a728d43cecbad03552fe0533ed23
aloirene <alomandell96@gmail.com>
2025-01-28 18:26:46 -0500

    changes made from theia lab environment

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,18 @@
-import LoginPanel from "./components/Login/Login"
+import React from "react";
 import { Routes, Route } from "react-router-dom";
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register";  // Import Register component
 
 function App() {
   return (
     <Routes>
+      {/* Route for Login page */}
       <Route path="/login" element={<LoginPanel />} />
+
+      {/* Route for Register page */}
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
+
 export default App;
